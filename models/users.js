401ceb5802dc99f9e16b5700c9f8247b94dae305
kelechi-aims <kelechi@aims.edu.gh>
2024-03-17 15:32:08 +0100

    message model

---
+++ models/users.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+/**
+ * User schema
+ * 
+ */
+
+//location schema. it will be a subdocument in user schema
+const locationSchema = new mongoose.Schema({
+    country: { type: String, required: true },
+    region: { type: String, required: true },
+    town: { type: String, required: true },
+    latitude: { type: Number, required: false, default: 0 },
+    longitude: { type: Number, required: false, default: 0 },
+}, { _id: false }) //disables auto generation of id's for this sub doc
+
+const userSchema = new mongoose.Schema({
+    firstname: { type: String, required: true },
+    secondname: { type: String, required: true },
+    course: { type: String, required: true },
+    cohort: { type: String, required: true },
+    email : { type: String, required: true },
+    password: { type: String, required: true },
+    location: { type: locationSchema, required: true }// i called the location sub doc defined above
+    
+}, { timestamps: true }) 
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
