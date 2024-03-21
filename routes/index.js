ca68f35896e8f396f14be3fcfa75c3fc2cf1e0a2
kelechi-aims <kelechi@aims.edu.gh>
2024-03-21 21:39:39 +0100

    sending and receiving messages between two pages using socket.io

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const UsersController = require('../controllers/usersController');
 const MessagesController = require('../controllers/messageController');
+const path = require('path');
 
 const injectRoutes = () => {
     const router = express.Router();
+    // router.get('/message', (req, res) => {
+    // res.sendFile(path.join(__dirname, '.', 'views', 'message.html'));
+    // });
     router.post('/users', UsersController.createUser);
     router.post('/messages', MessagesController.createMessage);
     router.get('/messages/:userID', MessagesController.getMessagesByUserID);
--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const injectRoutes = require('./routes/index.js');
 const DbClient = require('./utils/db.js');
 const express = require('express');
 const http = require('http');
-const socketIO = require('socket.io');
+const { Server } = require('socket.io');
+const path = require('path');
 // const MessageController = require('./controllers/messageController');
 
 const dbClient = new DbClient;
@@ -10,12 +11,18 @@ const dbClient = new DbClient;
 const app = express();
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '.', 'views', 'index.html'));
+});
+
 app.use('/api', injectRoutes());
 
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer(app);
-const io = socketIO(server);
+const io = new Server(server, {
+  connectionStateRecovery: {}
+});
 
 
 // Initialize MessageController with the HTTP server instance
@@ -23,12 +30,14 @@ const io = socketIO(server);
 
 // WebSocket event handling
 io.on('connection', (socket) => {
-  console.log('A user connected');
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
 
   // Handle disconnect event
-  socket.on('disconnect', () => {
-    console.log('User disconnected');
-  });
+  //socket.on('disconnect', () => {
+   // console.log('User disconnected');
+  //});
 });
 
 server.listen(PORT, () => {
